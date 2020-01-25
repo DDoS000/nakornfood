f12d88ca5c8c1bc0e458c7411d89d5a2461df682
DDoS000 <Suphamongkhon.kh.61@ubu.ac.th>
2020-01-26 00:09:15 +0700

    0

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,11 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return render_template('login.html')
 
+if __name__ == '__main__':
+    app.secret_key='DDoS'
+    app.run(debug=True)
