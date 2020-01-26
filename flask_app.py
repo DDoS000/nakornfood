625cce5955f639b25b166cafb7be6a3b5fdb2893
DDoS000 <Suphamongkhon.kh.61@ubu.ac.th>
2020-01-26 16:58:04 +0700

    tan

--- flask_app.py
+++ flask_app.py
@@ -19,27 +19,57 @@ firebase = pyrebase.initialize_app(fbConfig)
 auth = firebase.auth()
 database = firebase.database()
 
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Only Member, Please login !!!', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route('/')
 def index():
     return render_template('login.html')
 
+
+# Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('loginpass.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method =='POST':
-
         email = request.form['email']
         password = request.form['password']
 
         try:
             user = auth.sign_in_with_email_and_password(email,password)
+            # Passed
             session['logged_in'] = True
             session['uid'] = user["localId"]
-            session['email'] = user["dev@gm.com"]
-            return render_template('loginpass.html')
+            session['email'] = user["email"]
+            flash('You are now logged in', 'success')
+            return redirect(url_for('dashboard'))
         except Exception as Error:
-            error = str(Error)
-            return render_template('loginerror.html',error=error)
+            print(Error)
+            error = 'Wrong username or password !!!'
+            return render_template('login.html',error=error)
+    return render_template('login.html')
 
+
+
+# Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
 
 
 
